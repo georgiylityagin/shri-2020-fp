3789e3df99a8deb64ff02b60e88ad775febd223a
georgiylityagin <geolit94@gmail.com>
2020-04-14 01:47:04 +0300

    built for gh-pages

---
+++ build/precache-manifest.7407373f5e67d9cae3167da73cf21db1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56665c30ae09a0c56aec205dfc66a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e28801a788e62294f8",
+    "url": "/static/css/main.5ac42eff.chunk.css"
+  },
+  {
+    "revision": "98049b4a3285f94735ca",
+    "url": "/static/js/2.a7925d20.chunk.js"
+  },
+  {
+    "revision": "d9e28801a788e62294f8",
+    "url": "/static/js/main.e4ef152b.chunk.js"
+  },
+  {
+    "revision": "988edce19459a4d2553a",
+    "url": "/static/js/runtime-main.a5d4bab2.js"
+  }
+]);
